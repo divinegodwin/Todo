62c5ab77a764490c9a154307c2fcc9c641364117
divinegodwin172@gmail.com <divinegodwin172@gmail.com>
2022-06-16 19:34:59 +0100

    new features, validation

--- main.js
+++ main.js
@@ -1,5 +1,49 @@
+let modal = document.querySelector(".modal");
+let container1 = document.querySelector(".container1");
+let name = document.querySelector(".name");
+let nameBtn = document.querySelector(".name-btn");
+
+let p = document.querySelector(".input-name-value");
+
+let pContainer = document.querySelector("#validation");
+
+
+if(localStorage.getItem("validation") == null){
+window.onload =()=>{
+  
+  container1.classList.add("blur");
+  
+    nameBtn.addEventListener("click", ()=>{
+      
+        console.log("do not exist")
+        modal.style.display ="none";
+      if(name.value != ""){
+     
+    
+     container1.classList.remove("blur");
+      p.innerHTML = " Hello " + name.value;
+      
+      localStorage.setItem("validation", p.innerHTML);
+      
+      }
+    
+    })
+}
+
+}else{
+  console.log("exist");
+  modal.style.display="none";
+  p.innerHTML = " Hello" + name.value;
+  console.log(p.innerHTML)
+}
+  
+
+ p.innerHTML = localStorage.getItem("validation");
+
+console.log(p.innerHTML)
+
 //naming the Dom
-let input = document.querySelector(".text"),
+let input = document.querySelector(".text");
     addBtn = document.querySelector(".add"),
     systemTodo = document.querySelector(".system-todo"),
     todoContainer = document.querySelector("#todo-container");
@@ -7,7 +51,6 @@ let input = document.querySelector(".text"),
 
 addBtn.addEventListener("click", (e) => {
     if (input.value != "") {
-        let todos = document.createElement("li");
         todos.innerHTML = input.value;
         todoContainer.appendChild(todos);
         todos.classList.add("todos");
@@ -38,3 +81,4 @@ function clear_all() {
 /* Update the content of the
 todo container on start */
 todoContainer.innerHTML = localStorage.getItem("todoContainer");
+
